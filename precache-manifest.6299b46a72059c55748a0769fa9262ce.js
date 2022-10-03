a5cfc023c29976eb5bc4a961c15f0e913d99f825
SoloPane <me@solopane.me>
2022-10-04 03:05:50 +0600

    update

---
+++ precache-manifest.6299b46a72059c55748a0769fa9262ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a52e81cb86b6ee4e9b342f5c9ef92c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6233b4eccfcc269ebe59",
+    "url": "./static/js/2.20f6fdb0.chunk.js"
+  },
+  {
+    "revision": "3a1eda99c3ddee98f2f3655546a4ac02",
+    "url": "./static/js/2.20f6fdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5b609a7ed9a569af4d",
+    "url": "./static/js/main.f606369b.chunk.js"
+  },
+  {
+    "revision": "6a83ecb161aaf99d7381",
+    "url": "./static/js/runtime-main.2c55a0ab.js"
+  },
+  {
+    "revision": "439b7a4d84f58e5b55d1ff8a24cf1cc0",
+    "url": "./static/media/eth.439b7a4d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33e42a62325fbb11109cd3068c3d3b5f.js"
+  "./precache-manifest.6299b46a72059c55748a0769fa9262ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
