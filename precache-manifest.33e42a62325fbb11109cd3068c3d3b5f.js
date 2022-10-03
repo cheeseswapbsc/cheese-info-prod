9474c5a127485e89e4d4aced33542a568ba962d5
SoloPane <me@solopane.me>
2022-10-04 02:39:24 +0600

    oct2022

---
+++ precache-manifest.33e42a62325fbb11109cd3068c3d3b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f26ced351d557e1e7518b4ccf0630a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bff1fac5928f0e7c2058",
+    "url": "/static/js/2.336b72f1.chunk.js"
+  },
+  {
+    "revision": "3a1eda99c3ddee98f2f3655546a4ac02",
+    "url": "/static/js/2.336b72f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07756f0f33f07ed2677",
+    "url": "/static/js/main.0a022bd4.chunk.js"
+  },
+  {
+    "revision": "195868ec4782d30050e2",
+    "url": "/static/js/runtime-main.70e1907e.js"
+  },
+  {
+    "revision": "439b7a4d84f58e5b55d1ff8a24cf1cc0",
+    "url": "/static/media/eth.439b7a4d.png"
+  }
+]);
